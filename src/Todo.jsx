4cde2a6177230a8a9fa99b262aed12a33c630f6d
mahak <kaushikikes@gmail.com>
2026-02-25 21:49:48 +0530

    add photo

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,9 +1,20 @@
 function Todo() {
-    return (
-        <div>
-            hello todo welcome to react
-        </div>
-    );
+  return (
+    <div>
+      hello todo welcome to react
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Todo;
